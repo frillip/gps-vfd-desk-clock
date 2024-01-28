4327df78af98c024005208b85a6eec7a85a4abc7
Phil Martin <root@frillip.com>
2024-01-28 21:12:33 +0000

    Added menu function

--- src/tubes.h
+++ src/tubes.h
@@ -107,6 +107,7 @@ uint64_t display_generate_buffer(uint16_t digits);
 void display_send_buffer(uint64_t buffer);
 void display_time(const time_t *tod);
 void display_mmss(const time_t *tod);
+void display_menu(void);
 void display_dashes(void);
 void display_blank(void);
 void display_latch(void);
