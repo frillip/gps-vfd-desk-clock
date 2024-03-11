11df76d9e0aff97ac5b5c8657cf7974f221c5e50
Phil Martin <root@frillip.com>
2024-03-11 01:47:02 +0000

    Update system.c comment

--- dspic_src/mcc_generated_files/system.c
+++ dspic_src/mcc_generated_files/system.c
@@ -54,7 +54,7 @@
 #pragma config WDTWIN = WIN25    //Watchdog Window Select bits->WDT Window is 25% of WDT period
 
 // FWDT
-#pragma config WDTPOST = PS8192    //Watchdog Timer Postscaler bits->1:32768
+#pragma config WDTPOST = PS8192    //Watchdog Timer Postscaler bits->1:8192
 #pragma config WDTPRE = PR128    //Watchdog Timer Prescaler bit->1:128
 #pragma config PLLKEN = ON    //PLL Lock Enable bit->Clock switch to PLL source will wait until the PLL lock signal is valid.
 #pragma config WINDIS = OFF    //Watchdog Timer Window Enable bit->Watchdog Timer in Non-Window mode
