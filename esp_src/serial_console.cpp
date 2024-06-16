6f20e95d7b5107fc674f1fde4e7fd0fadd537860
Phil Martin <root@frillip.com>
2024-06-16 23:34:57 +0100

    Beginnings of a serial console

--- esp_src/serial_console.cpp
+++ esp_src/serial_console.cpp
@@ -1,5 +1,8 @@
 #include "serial_console.h"
 
+char console_buffer[SERIAL_CONSOLE_BUFFER_LENGTH] = {0};
+uint8_t console_buffer_offset = 0;
+
 void serial_console_init()
 {
     Serial.begin(DEBUG_BAUD);
@@ -12,41 +15,66 @@ bool serial_console_char_available(void)
 
 void serial_console_task(void)
 {
-  extern WiFiManager wm;
   if(Serial.available())
   {
     char c = Serial.read();
     Serial.print(c);
-    if(c==0x45) ESP.restart(); // Reset ESP on 'E'
-    else if(c==0x57) // on 'W'...
+
+    console_buffer[console_buffer_offset] = c;
+
+    if(c == 0x0A || c == 0x0D) // new line char
     {
-      wm.resetSettings(); // Delete WiFi credentials
-      ESP.restart(); // And reset
+      // Just exec first character in buffer for now
+      // To do: implement two stage buffer check and proper commands
+      // Stage 1: check first 4 bytes for 'esp-' or 'pic-' or first 5 bytes for 'reset'
+      // Stage 2: check against entire command string in serial_cmds.txt
+      // Stage 3: run command and parse options (if required)
+      serial_console_exec(console_buffer[0]);
+      console_buffer_offset = 0;      
     }
-    else if(c==0x5A) // on 'Z'
+    else
     {
-      Serial.println("");
-      print_pic_time();
-      
-      if(gnss_is_detected() && timeStatus() != timeNotSet)
+      console_buffer_offset++;
+      if(console_buffer_offset > SERIAL_CONSOLE_BUFFER_LENGTH -1)
       {
-        print_gnss_pps_offset();
+        console_buffer_offset = 0;
       }
-      if(pic_is_detected() && timeStatus() != timeNotSet)
-      {
-        print_pic_pps_offset();
-      }
-      
-      print_offset_data();
-      print_gnss_data();
-      pic_print_rtc();
-      print_veml_data();
-      print_bme_data();
-      print_sync_state_machine();
     }
-    USER_CMD cmd = static_cast<USER_CMD>(c);
-    pic_uart_tx_userdata(cmd, 0);
   }
+}
+
+void serial_console_exec(char c)
+{
+  if(c==0x45) ESP.restart(); // Reset ESP on 'E'
+  else if(c==0x57) // on 'W'...
+  {
+    extern WiFiManager wm;
+    wm.resetSettings(); // Delete WiFi credentials
+    ESP.restart(); // And reset
+  }
+  else if(c==0x5A) // on 'Z'
+  {
+    Serial.println("");
+    print_pic_time();
+    
+    if(gnss_is_detected() && timeStatus() != timeNotSet)
+    {
+      print_gnss_pps_offset();
+    }
+    if(pic_is_detected() && timeStatus() != timeNotSet)
+    {
+      print_pic_pps_offset();
+    }
+    
+    print_offset_data();
+    print_gnss_data();
+    pic_print_rtc();
+    print_veml_data();
+    print_bme_data();
+    print_sync_state_machine();
+  }
+  USER_CMD cmd = static_cast<USER_CMD>(c);
+  pic_uart_tx_userdata(cmd, 0);
 }
 
 uint32_t local_print_millis = 0;
@@ -58,6 +86,9 @@ void serial_console_second_changed(uint32_t millis)
 
 bool serial_console_print_local_available(void)
 {
+  // Disable this for now
+  return 0;
+
   if(local_print_millis)
   {
     if(millis() > (local_print_millis + LOCAL_TIME_PRINT_DELAY))
--- esp_src/serial_console.h
+++ esp_src/serial_console.h
@@ -12,6 +12,7 @@
 #include <HardwareSerial.h>
 
 #define LOCAL_TIME_PRINT_DELAY 50
+#define SERIAL_CONSOLE_BUFFER_LENGTH 100
 
 #define DEBUG_UART  0
 #define DEBUG_BAUD  115200
@@ -19,6 +20,8 @@
 void serial_console_init();
 bool serial_console_char_available(void);
 void serial_console_task(void);
+void serial_console_exec(char c);
+
 void serial_console_second_changed(uint32_t millis);
 bool serial_console_print_local_available(void);
 void serial_console_print_local(void);
